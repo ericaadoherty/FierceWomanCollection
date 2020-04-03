fa625a8db041edec1b36f79b0c18f7ede3d741ff
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-04-02 21:47:35 -0500

    I created a controller titled Product controller. I also created a viewimports html and added a taghelper. then i created a product class with my different variables. and then created a fake repository with 4 products

---
+++ Fierce/Fierce/Models/FakeProductReposistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fierce.Models
+{
+    public class FakeProductReposistory : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+        new Product { Name = "Fierce & Favored", Price = 28 },
+        new Product { Name = "Fierce Woman Entrepreneur", Price = 28 },
+        new Product { Name = "Fierce Definition", Price = 28 },
+        new Product { Name = "Fierce Woman of Faith" , Price = 28}
+        }.AsQueryable<Product>();
+    }
+}
---
+++ Fierce/Fierce/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fierce.Models
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
