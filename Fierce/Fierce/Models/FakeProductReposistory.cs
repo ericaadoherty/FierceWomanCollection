5ab4ed81c938d8960f739c02f4081ebc5d083363
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-04-08 19:38:29 -0500

    Finished adding products to fake repository

--- Fierce/Fierce/Models/FakeProductReposistory.cs
+++ Fierce/Fierce/Models/FakeProductReposistory.cs
@@ -11,7 +11,15 @@ namespace Fierce.Models
         new Product { Name = "Fierce & Favored", Price = 28 },
         new Product { Name = "Fierce Woman Entrepreneur", Price = 28 },
         new Product { Name = "Fierce Definition", Price = 28 },
-        new Product { Name = "Fierce Woman of Faith" , Price = 28}
+        new Product { Name = "Fierce Woman of Faith" , Price = 28},
+        new Product {Name ="Fierce & Favored Sweatshirt", Price = 40},
+        new Product{Name ="Fierce Woman Entrepreneur Sweatshirt", Price = 40},
+        new Product{Name ="Fierce Definition Sweatshirt", Price = 40},
+        new Product{Name ="Fierce Woman of Faith Sweatshirt", Price = 40},
+        new Product {Name ="Fierce & Favored Hoodie", Price = 45},
+        new Product{Name ="Fierce Woman Entrepreneur Hoodie", Price = 45},
+        new Product{Name ="Fierce Definition Hoodie", Price = 45},
+        new Product{Name ="Fierce Woman of Faith Hoodie", Price = 45}
         }.AsQueryable<Product>();
     }
 }
