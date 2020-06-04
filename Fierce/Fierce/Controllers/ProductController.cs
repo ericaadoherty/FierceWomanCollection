b73e77f3cc3cf00ffcd74872c0d2bce50d426d20
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-06-03 21:14:51 -0500

    made a few small changes

--- Fierce/Fierce/Controllers/ProductController.cs
+++ Fierce/Fierce/Controllers/ProductController.cs
@@ -10,13 +10,10 @@ namespace Fierce.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        public ProductController(IProductRepository repo)
-        {
-            repository = repo;
+        //public ProductController(IProductRepository repo)
+        //{
+            //repository = repo;
         }
-        public ViewResult List(int productPage = 1)
-    => View(repository.Products.OrderBy(p => p.ProductID)
-     .Skip((productPage - 1) * PageSize)
-    .Take(PageSize));
+        
     }
-}
+//}
