c8d42a0e8f9c398028285605c3dc75f52046d7b5
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-06-11 18:38:01 -0500

    a couple of small changes, didnt get as much done today kind of have a set back and seekeing advice from another teacher

--- Fierce/Fierce/Startup.cs
+++ Fierce/Fierce/Startup.cs
@@ -22,10 +22,9 @@ namespace Fierce
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
-            services.AddTransient<IProductRepository, FakeProductRepository>();
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:FierceProducts:ConnectionString"]));
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+           // services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
