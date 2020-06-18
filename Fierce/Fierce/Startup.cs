f83dd17df5e9589a2ee0d972abaf27abd550ad41
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-06-17 21:25:31 -0500

    changed reference and also got rid of error

--- Fierce/Fierce/Startup.cs
+++ Fierce/Fierce/Startup.cs
@@ -22,12 +22,12 @@ namespace Fierce
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:FierceProducts:ConnectionString"]));
-            //services.AddTransient<IProductRepository, FakeProductRepository>();
-           // services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:FierceProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
