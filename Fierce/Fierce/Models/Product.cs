63fba9ef2f124c8357a1bf7408900191709430c7
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-07-22 20:45:33 -0500

    Fixed the remove button on the shopping cart

--- Fierce/Fierce/Models/Product.cs
+++ Fierce/Fierce/Models/Product.cs
@@ -11,7 +11,7 @@ namespace Fierce.Models
         public int ProductID { get; set; }
         [Required(ErrorMessage = "Please enter a product name")] 
         public string Name { get; set; }
-        [Required(ErrorMessage = "Please enter a description")]
+       // [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
         [Required]
         [Range(0.01, double.MaxValue,
