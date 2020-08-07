a119dc52f46fc4077089992f1305c60550e6fef6
Erica Doherty <edoherty021827@students.iticollege.edu>
2020-08-06 20:25:13 -0500

    Finished project

--- Fierce/Fierce/Controllers/ProductController.cs
+++ Fierce/Fierce/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace Fierce.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        public int PageSize = 4;
+        public int PageSize = 12;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
